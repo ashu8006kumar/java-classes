fa69e47eac9be2ca6a9057fee238f1540167dc42
Ashish <ashu8006kumar@gmail.com>
2021-05-14 21:49:04 +0530

    car and control class for git demo

---
+++ src/io/arha/day/two/Car.java
@@ -0,0 +1,15 @@
+package io.arha.day.two;
+
+public class Car {
+	static int numberOfCars = 0; 
+	
+	String name;
+	String color;
+
+	public Car(String name, String color) {
+		super();
+		this.name = name;
+		this.color = color;
+	}
+
+}
