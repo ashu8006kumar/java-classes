6c76419bd1105af480d796181b33555506ef7391
Ashish <ashish@biztecno.net>
2021-05-14 20:25:12 +0530

    2 D array

---
+++ src/io/arha/day/four/Array2D.java
@@ -0,0 +1,28 @@
+package io.arha.day.four;
+
+public class Array2D {
+	public static void main(String[] arg) {
+		
+		
+		String[][] arr = new String[2][3];
+
+		for (int row = 0; row < arr.length; row++) {
+
+			for (int col = 0; col < arr[row].length; col++) {
+				arr[row][col] = row + "-" + col;
+
+			}
+			System.out.println();
+		}
+
+		for (int row = 0; row < arr.length; row++) {
+
+			for (int col = 0; col < arr[row].length; col++) {
+				System.out.print(arr[row][col]+"  ");
+
+			}
+			System.out.println();
+		}
+	}
+
+}
