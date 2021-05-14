a5e66e2e3f33066d023d13ef9ad153ca00be0d41
Ashish <ashu8006kumar@gmail.com>
2021-05-14 20:31:44 +0530

    updated

---
+++ src/io/arha/day/three/Calculator.java
@@ -0,0 +1,54 @@
+package io.arha.day.three;
+
+public class Calculator {
+
+	private double number1;
+	private double number2;
+	private double result = 0;
+	private char opration;
+
+	public Calculator(double number1, double number2, char opration) {
+		super();
+		this.number1 = number1;
+		this.number2 = number2;
+		this.opration = opration;
+	}
+
+	public double calculate() {
+		switch (opration) {
+		case '+':
+			System.out.println("in addition");
+			result = number1 + number2;
+			break;
+		case '-':
+			System.out.println("in subtraction");
+			result = number1 - number2;
+			break;
+		case '*':
+			System.out.println("in multiplication");
+			result = number1 * number2;
+			break;
+		case '/':
+			System.out.println("in devision");
+			try { 
+				result = number1 / number2;
+			} catch (ArithmeticException ae) { 
+				throw ae;
+
+			}
+			break;
+
+		default:
+			System.out.println("Undefined opration.");
+			break;
+		}
+		System.out.println("End of switch case");
+		return result;
+	}
+
+	public static void main(String[] agr) {
+		Calculator calculator = new Calculator(12.0, 0, '/');
+		System.out.println(calculator.calculate());
+
+	}
+}
